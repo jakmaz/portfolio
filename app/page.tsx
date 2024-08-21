4876a29660626fdecbf0c55099f28c47a991d14a
Jakub Mazur <j.mazur.nl@gmail.com>
2024-08-21 10:47:19 +0200

    ✨ Add experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import { Experience } from "@/components/experience";
 import Projects from "@/components/projects";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-12">
       <p className="text-gray-500">
         Driven by a passion for technology and development, dedicated to
         creating impactful software that makes a difference in people’s lives.
       </p>
+      <Experience />
       <Projects />
     </div>
   );
---
+++ components/experience.tsx
@@ -0,0 +1,44 @@
+export const Experience = () => {
+  return (
+    <div>
+      <h1 className="font-medium text-gray-900 mb-4 text-lg">Education</h1>
+      <ol className="relative border-s border-gray-200">
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border"></div>
+          <div className="flex flex-row items-center gap-2">
+            <div className="text-md font-medium text-gray-900">
+              Computer Science
+            </div>
+            <div className="inline-block uppercase text-xs rounded-full px-1 py-0 border border-gray-500 text-gray-500">
+              present
+            </div>
+          </div>
+          <div className="mb-4 text-sm font-normal text-gray-500">
+            <a
+              href="https://www.maastrichtuniversity.nl/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Maastricht University
+            </a>
+          </div>
+        </li>
+        <li className="ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border"></div>
+          <div className="text-md font-medium text-gray-900">
+            Automation Technician
+          </div>
+          <div className="mb-4 text-sm font-normal text-gray-500">
+            <a
+              href="https://www.zspkleszczow.pl/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Secondary Technical School of Modern Technologies
+            </a>
+          </div>
+        </li>
+      </ol>
+    </div>
+  );
+};
