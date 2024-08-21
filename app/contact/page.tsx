380ec15b26f7a6eb12a379afa91473c92efc9c73
Jakub Mazur <j.mazur.nl@gmail.com>
2024-08-21 11:36:07 +0200

    🔧 Fix linting problem

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <div className="section-description flex items-center space-x-2">
-      <h1>Let's work on something together, drop me a message here:</h1>
+      <h1>Let &apos s work on something together, drop me a message here:</h1>
       <a
         href="mailto:j.mazur.nl@gmail.com"
         className="text-black hover:text-gray-500"
