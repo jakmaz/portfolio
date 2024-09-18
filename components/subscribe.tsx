5870e48e1b47ecc7be21ba6c7f06801778a62632
Jakub Mazur <j.mazur.nl@gmail.com>
2024-09-18 22:30:00 +0200

    🧹 Add the action response confirming subscription

--- components/subscribe.tsx
+++ components/subscribe.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import { addNewsletterSubscriber } from "@/lib/actions";
+import { useActionState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 export default function Subscribe() {
+  type NewsletterResponse = {
+    message: string;
+  };
+
+  const [respose, formAction, isPending] = useActionState(
+    addNewsletterSubscriber,
+    undefined,
+  );
   return (
-    <form action={addNewsletterSubscriber} className="flex gap-2">
-      <Input name="email" type="email" placeholder="Email Address" />
-      <Button className="bg-gray-800">Subscribe</Button>
+    <form action={formAction} className="flex flex-col gap-2">
+      <div className="flex gap-2">
+        <Input name="email" type="email" placeholder="Email Address" />
+        <Button className="bg-gray-800" disabled={isPending}>
+          Subscribe
+        </Button>
+      </div>
+      <p className="text-gray-600"> {respose?.message} </p>
     </form>
   );
 }
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-export async function addNewsletterSubscriber(formData: FormData) {
+export async function addNewsletterSubscriber(_: any, formData: FormData) {
   const email = formData.get("email");
 
   const url = new URL("https://api.sender.net/v2/subscribers");
@@ -16,12 +16,29 @@ export async function addNewsletterSubscriber(formData: FormData) {
     trigger_automation: false,
   };
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers,
-    body: JSON.stringify(data),
-  });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data),
+    });
 
-  const responeData = await response.json();
-  console.log(responeData);
+    const responseData = await response.json();
+
+    if (response.ok && responseData.success) {
+      return {
+        message: "Subscription successful!",
+      };
+    } else {
+      console.error("Subscription failed:", responseData);
+      return {
+        message: "Subscription failed. Please try again.",
+      };
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return {
+      message: "An error occurred. Please try again later.",
+    };
+  }
 }
