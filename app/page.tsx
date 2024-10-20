53ad11f38c0cfb83e22804ea77b162880aee8937
Jakub Mazur <j.mazur.nl@gmail.com>
2024-10-20 23:28:23 +0200

    ✨ Technologies section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Experience } from "@/components/experience";
 import JournalSection from "@/components/journal-section";
 import LatestBlogs from "@/components/latest-blogs";
 import Projects from "@/components/projects";
+import TechStack from "@/components/tech-stack";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         creating impactful software that makes a difference in people&apos;s
         lives.
       </p>
+      <TechStack />
       <Experience />
       <Projects />
       <LatestBlogs />
---
+++ components/tech-stack.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+const technologies = [
+  { name: "Next.js", icon: "nextjs" },
+  { name: "React", icon: "react" },
+  { name: "Tailwind CSS", icon: "tailwindcss" },
+  { name: "Docker", icon: "docker" },
+  { name: "Node.js", icon: "nodejs" },
+  { name: "TypeScript", icon: "typescript" },
+  { name: "PostreSQL", icon: "postgresql" },
+  { name: "Java", icon: "java" },
+];
+
+export default function TechStack() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="section-title">Technologies</h2>
+      <div className="grid grid-cols-4 gap-4">
+        {technologies.map((tech, index) => (
+          <div key={index} className="flex items-center gap-2 justfy">
+            <Image
+              src={`/images/icons/${tech.icon}.svg`}
+              alt={tech.name}
+              width={24}
+              height={24}
+            />
+            <p className="text-lg text-gray-700">{tech.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
