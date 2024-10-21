6acc327a0cc98a41a5f52180fcb82421e94de7ee
Jakub Mazur <j.mazur.nl@gmail.com>
2024-10-21 11:56:02 +0200

    💄 Change hero layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     <html lang="en" className={playfair.variable}>
       <body>
         <main className="flex flex-col items-center justify-between w-full p-8 min-h-screen">
-          <div className="flex flex-col gap-8 w-full max-w-3xl">
+          <div className="flex flex-col gap-6 w-full max-w-3xl">
             <Hero />
             <Separator />
             <Navbar />
--- components/hero.tsx
+++ components/hero.tsx
@@ -20,28 +20,32 @@ const socialLinks = [
 
 export default function Hero() {
   return (
-    <div className="flex flex-col">
-      <h1 className="font-medium text-gray-900 text-xl">Jakub Mazur</h1>
-      <p>CS Student &amp; Full Stack Developer</p>
-      <p className="text-gray-700">Maastricht - Netherlands</p>
-      <div className="flex flex-row gap-3 mt-4">
-        {socialLinks.map((link, index) => (
-          <a
-            key={index}
-            href={link.href}
-            target="_blank"
-            rel="noreferrer"
-            className="transition-transform transform hover:scale-110"
-          >
-            <Image
-              alt={link.alt}
-              src={link.src}
-              width={24}
-              height={24}
-              priority={true}
-            />
-          </a>
-        ))}
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-col">
+        <h1 className="font-medium text-gray-900 text-xl">Jakub Mazur</h1>
+        <p>CS Student &amp; Full Stack Developer</p>
+      </div>
+      <div className="flex flex-col gap-2 items-end">
+        <p className="text-gray-700">Maastricht - Netherlands</p>
+        <div className="flex flex-row gap-3 ">
+          {socialLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noreferrer"
+              className="transition-transform transform hover:scale-110"
+            >
+              <Image
+                alt={link.alt}
+                src={link.src}
+                width={24}
+                height={24}
+                priority={true}
+              />
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
