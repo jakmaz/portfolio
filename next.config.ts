9648b82951805a9bcd1632fe0836301b01ded002
Jakub Mazur <j.mazur.nl@gmail.com>
2024-10-23 18:36:06 +0200

    🧑💻 Add cache logging in development

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = { logging: { fetches: { fullUrl: true } } };
 
 export default nextConfig;
